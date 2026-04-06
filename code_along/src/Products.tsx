3b582fb37bf57310bbbc75b778e7d5caa5bbffd0
Pedro Dejesus <dejesuspedro07@gmail.com>
2026-04-05 23:27:50 -0400

    Added products to nav

---
+++ code_along/src/Products.tsx
@@ -0,0 +1,36 @@
+function Products(){
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>Cost</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Apple</td>
+                    <td>Fruit</td>
+                    <td>2.50</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Pineapple</td>
+                    <td>Fruit</td>
+                    <td>4.50</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Cucummber</td>
+                    <td>Vegitable</td>
+                    <td>5400</td>
+                </tr>
+            </tbody>
+
+        </table>
+    )
+}
+
+export default Products
